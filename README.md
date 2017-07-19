ff6152c4f7ed3c62d8e1225c2eb801c9fc2f8153
Radek Stepan <dev@radekstepan.com>
2017-07-18 20:57:51 -0400

    move to docs

---
+++ README.md
@@ -0,0 +1,9 @@
+# radekstepan.com
+
+A personal page & blog running on [Metalsmith](http://www.metalsmith.io/) with [Nunjucks](https://mozilla.github.io/nunjucks/) templates.
+
+![image](https://raw.githubusercontent.com/radekstepan/radekstepan.github.io/src/example.png)
+
+```bash
+$ git checkout src
+```
---
+++ assets/templates/posts.js
@@ -0,0 +1,260 @@
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["posts.html"] = (function() {
+function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+var parentTemplate = null;
+output += "<ul id=\"posts\">\n  ";
+frame = frame.push();
+var t_3 = runtime.contextOrFrameLookup(context, frame, "posts");
+if(t_3) {var t_2 = t_3.length;
+for(var t_1=0; t_1 < t_3.length; t_1++) {
+var t_4 = t_3[t_1];
+frame.set("post", t_4);
+frame.set("loop.index", t_1 + 1);
+frame.set("loop.index0", t_1);
+frame.set("loop.revindex", t_2 - t_1);
+frame.set("loop.revindex0", t_2 - t_1 - 1);
+frame.set("loop.first", t_1 === 0);
+frame.set("loop.last", t_1 === t_2 - 1);
+frame.set("loop.length", t_2);
+output += "\n    <li>\n      <h2><a href=\"/";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"path"), env.opts.autoescape);
+output += "\" style=\"color:";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"color"), env.opts.autoescape);
+output += "\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"title"), env.opts.autoescape);
+output += "</a></h2>\n      <div class=\"date\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"date"), env.opts.autoescape);
+output += "</div>\n    </li>\n  ";
+;
+}
+}
+frame = frame.pop();
+output += "\n</ul>\n";
+if(parentTemplate) {
+parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
+} else {
+cb(null, output);
+}
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+
+})();
+})();
+
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["posts.html"] = (function() {
+function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+var parentTemplate = null;
+output += "<ul id=\"posts\">\n  ";
+frame = frame.push();
+var t_3 = runtime.contextOrFrameLookup(context, frame, "posts");
+if(t_3) {var t_2 = t_3.length;
+for(var t_1=0; t_1 < t_3.length; t_1++) {
+var t_4 = t_3[t_1];
+frame.set("post", t_4);
+frame.set("loop.index", t_1 + 1);
+frame.set("loop.index0", t_1);
+frame.set("loop.revindex", t_2 - t_1);
+frame.set("loop.revindex0", t_2 - t_1 - 1);
+frame.set("loop.first", t_1 === 0);
+frame.set("loop.last", t_1 === t_2 - 1);
+frame.set("loop.length", t_2);
+output += "\n    <li>\n      <h2><a href=\"/";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"path"), env.opts.autoescape);
+output += "\" style=\"color:";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"color"), env.opts.autoescape);
+output += "\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"title"), env.opts.autoescape);
+output += "</a></h2>\n      <div class=\"date\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"date"), env.opts.autoescape);
+output += "</div>\n    </li>\n  ";
+;
+}
+}
+frame = frame.pop();
+output += "\n</ul>\n";
+if(parentTemplate) {
+parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
+} else {
+cb(null, output);
+}
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+
+})();
+})();
+
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["posts.html"] = (function() {
+function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+var parentTemplate = null;
+output += "<ul id=\"posts\">\n  ";
+frame = frame.push();
+var t_3 = runtime.contextOrFrameLookup(context, frame, "posts");
+if(t_3) {var t_2 = t_3.length;
+for(var t_1=0; t_1 < t_3.length; t_1++) {
+var t_4 = t_3[t_1];
+frame.set("post", t_4);
+frame.set("loop.index", t_1 + 1);
+frame.set("loop.index0", t_1);
+frame.set("loop.revindex", t_2 - t_1);
+frame.set("loop.revindex0", t_2 - t_1 - 1);
+frame.set("loop.first", t_1 === 0);
+frame.set("loop.last", t_1 === t_2 - 1);
+frame.set("loop.length", t_2);
+output += "\n    <li>\n      <h2><a href=\"/";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"path"), env.opts.autoescape);
+output += "\" style=\"color:";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"color"), env.opts.autoescape);
+output += "\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"title"), env.opts.autoescape);
+output += "</a></h2>\n      <div class=\"date\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"date"), env.opts.autoescape);
+output += "</div>\n    </li>\n  ";
+;
+}
+}
+frame = frame.pop();
+output += "\n</ul>\n";
+if(parentTemplate) {
+parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
+} else {
+cb(null, output);
+}
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+
+})();
+})();
+
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["posts.html"] = (function() {
+function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+var parentTemplate = null;
+output += "<ul id=\"posts\">\n  ";
+frame = frame.push();
+var t_3 = runtime.contextOrFrameLookup(context, frame, "posts");
+if(t_3) {var t_2 = t_3.length;
+for(var t_1=0; t_1 < t_3.length; t_1++) {
+var t_4 = t_3[t_1];
+frame.set("post", t_4);
+frame.set("loop.index", t_1 + 1);
+frame.set("loop.index0", t_1);
+frame.set("loop.revindex", t_2 - t_1);
+frame.set("loop.revindex0", t_2 - t_1 - 1);
+frame.set("loop.first", t_1 === 0);
+frame.set("loop.last", t_1 === t_2 - 1);
+frame.set("loop.length", t_2);
+output += "\n    <li>\n      <h2><a href=\"/";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"path"), env.opts.autoescape);
+output += "\" style=\"color:";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"color"), env.opts.autoescape);
+output += "\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"title"), env.opts.autoescape);
+output += "</a></h2>\n      <div class=\"date\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"date"), env.opts.autoescape);
+output += "</div>\n    </li>\n  ";
+;
+}
+}
+frame = frame.pop();
+output += "\n</ul>\n";
+if(parentTemplate) {
+parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
+} else {
+cb(null, output);
+}
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+
+})();
+})();
+
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["posts.html"] = (function() {
+function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+var parentTemplate = null;
+output += "<ul id=\"posts\">\n  ";
+frame = frame.push();
+var t_3 = runtime.contextOrFrameLookup(context, frame, "posts");
+if(t_3) {var t_2 = t_3.length;
+for(var t_1=0; t_1 < t_3.length; t_1++) {
+var t_4 = t_3[t_1];
+frame.set("post", t_4);
+frame.set("loop.index", t_1 + 1);
+frame.set("loop.index0", t_1);
+frame.set("loop.revindex", t_2 - t_1);
+frame.set("loop.revindex0", t_2 - t_1 - 1);
+frame.set("loop.first", t_1 === 0);
+frame.set("loop.last", t_1 === t_2 - 1);
+frame.set("loop.length", t_2);
+output += "\n    <li>\n      <h2><a href=\"/";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"path"), env.opts.autoescape);
+output += "\" style=\"color:";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"color"), env.opts.autoescape);
+output += "\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"title"), env.opts.autoescape);
+output += "</a></h2>\n      <div class=\"date\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"date"), env.opts.autoescape);
+output += "</div>\n    </li>\n  ";
+;
+}
+}
+frame = frame.pop();
+output += "\n</ul>\n";
+if(parentTemplate) {
+parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
+} else {
+cb(null, output);
+}
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+
+})();
+})();
+
